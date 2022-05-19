4f4285f4e448824bb06ce5e5b2c13693c851b1f1
Barmanturbo <21035407@student.hhs.nl>
2022-05-19 14:22:36 +0200

    vergelijk user uitstoot met de gemeente amsterdam

--- src/main/java/com/logic/User.java
+++ src/main/java/com/logic/User.java
@@ -30,4 +30,30 @@ public class User {
     public int getRank(){
         return this.rank;
     }
+
+    public String vergelijkPuntMetUitstoot(){
+        long punten = point.getPoints();
+        String output = "Je CO2 uitstoot komt overeen met ";
+        if(punten<9){
+            output += "wat 1 vrachtwagen per km uitstoot";
+            return output;
+        }
+        if(punten <28){
+            output += "wat 1 vrachtwagen uitstoot per " + (punten / 9)+ " km";
+            return output;
+        }
+        if(punten < 20459){
+            
+            output += "wat " + (punten / 28) + " bomen per jaar opnemen";
+            return output;
+        }
+        if(punten < 122000000){
+            output += "wat de Ever Given uitstoot per " + (punten / 20459) + " km";
+            return output;
+        }else{
+            output += ""+(punten/122000000)+" keer zoveel dan wat de gemeente Amsterdam uitstootte in 2020";
+        }
+        
+        return output;
+    }
 }
