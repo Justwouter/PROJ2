4d40526ada067c74e6d43185e1386e863106e322
Justwouter <wouter2312@gmail.com>
2022-05-19 16:03:03 +0200

    Graph loads at fxml init, graph has colors &  Graph animates (sorta) correctly

--- src/main/java/com/gui/DashController.java
+++ src/main/java/com/gui/DashController.java
@@ -2,6 +2,8 @@ package com.gui;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import com.logic.User;
@@ -10,10 +12,13 @@ import javafx.beans.binding.ObjectExpression;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.StackedBarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
+
 import javafx.scene.control.Label;
+
 import javafx.scene.layout.Pane;
 
 public class DashController implements Initializable, IController{
@@ -27,9 +32,13 @@ public class DashController implements Initializable, IController{
     @FXML
     private Label pointsDash;
 
+    @FXML
+    private NumberAxis weekChartY;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        updateWeeklyChart();
 
     }
 
@@ -69,26 +78,47 @@ public class DashController implements Initializable, IController{
      */
     private void updateWeeklyChart(){
         //ArrayList<Object> historicUserData = new ArrayList<>();
+        List<Long> averageList = new ArrayList<>();
         String[] daysOfTheWeek = {"Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",};
         XYChart.Series<String,Number> series = new XYChart.Series<String, Number>();
 
         System.out.println("Parsing data");//Debug
 
         for(int i=0;i< daysOfTheWeek.length;i++){
-            Data<String,Number> vars = new XYChart.Data<String, Number>(daysOfTheWeek[i],Math.round(Math.random()*100));
+            Long userDataValue = Math.round(Math.random()*100);
+            averageList.add(userDataValue);
+            Data<String,Number> vars = new XYChart.Data<String, Number>(daysOfTheWeek[i],userDataValue);
             vars.setNode(createDataNode(vars.YValueProperty()));
+            colorSwitch(vars);
             series.getData().add(vars);
         }
 
+        //Calculate the average for the given datases
+        long average = 0;
+        long highest = 0;
+        for(Long l : averageList){
+            average +=l;
+            if(l > highest){
+                highest = l;
+            }
+        }
+        average = average/averageList.size();
+
+
+        //Debug
+        System.out.println("Average: "+average);
         System.out.println("Displaying data");
-        //series.setName("Weekly CO2 Discharge");
-        
+
+        //Assign lables
+        co2ThisWeekChart.setTitle("Weekly CO2 Discharge");
         co2ThisWeekChart.getXAxis().setLabel("Day");
         co2ThisWeekChart.getYAxis().setLabel("Value");
         
+        //Load the data & Toggle animations to avoid 'Issues'
         co2ThisWeekChart.setAnimated(false);
         co2ThisWeekChart.getData().clear();
         co2ThisWeekChart.getData().add(series);
+        weekChartY.setUpperBound(highest+20.0);
         co2ThisWeekChart.setAnimated(true);
         co2ThisWeekChart.setLegendVisible(false);
     }
@@ -108,5 +138,30 @@ public class DashController implements Initializable, IController{
         return pane;
     }
 
-    
+
+    /**
+     * Generates node colors for the given XYChart data values
+     * @param  data XYChart.Data
+     */
+    private void colorSwitch(XYChart.Data<String, Number> data){
+        Node node = data.getNode();
+        if (data.getYValue().intValue() > 99) {
+            node.setStyle("-fx-bar-fill: black");
+        } 
+        else if (data.getYValue().intValue() > 83) {
+            node.setStyle("-fx-bar-fill: red");
+        } 
+        else if (data.getYValue().intValue() > 67) {
+            node.setStyle("-fx-bar-fill: orange");
+        } 
+        else if (data.getYValue().intValue() > 50) {
+            node.setStyle("-fx-bar-fill: yellow");
+        }
+        else if (data.getYValue().intValue() > 33) {
+            node.setStyle("-fx-bar-fill: blue");
+        }
+        else {
+            node.setStyle("-fx-bar-fill: green");
+        }
+    }
 }
