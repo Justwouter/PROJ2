f468d030efbab31e21eb9772fe20b332e194199b
Justwouter <wouter2312@gmail.com>
2022-05-20 16:17:06 +0200

    Restore point

--- src/main/java/com/gui/DashController.java
+++ src/main/java/com/gui/DashController.java
@@ -12,8 +12,11 @@ import javafx.beans.binding.ObjectExpression;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
-import javafx.scene.chart.StackedBarChart;
+//import javafx.scene.chart.StackedBarChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.chart.XYChart.Data;
 
@@ -29,19 +32,28 @@ public class DashController implements Initializable, IController{
     private Label uitstootVergelijk;
 
     @FXML
-    private StackedBarChart<String,Number> co2ThisWeekChart;
+    private BarChart<String,Number> co2ThisWeekChart;
+
+    @FXML
+    private LineChart<String,Number> medianLineChart;
 
     @FXML
     private Label pointsDash;
 
     @FXML
-    private NumberAxis weekChartY;
+    private NumberAxis weekChartY = new NumberAxis();
+
+    @FXML
+    private CategoryAxis weekChartX = new CategoryAxis();
 
   
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         updateWeeklyChart();
     }
+
+    @Override
+    public void setPresets(User user){} //Empty on purpose, needed to implement the interface
 
     @FXML
     private void switchToReisGegevens() throws IOException {
@@ -72,16 +84,82 @@ public class DashController implements Initializable, IController{
      */
     @FXML
     public void triggerChartUpdate(){
-        updateWeeklyChart();
+        updateMedianLine(updateWeeklyChart());
         setVergelijking();
     }
+
+
+    private void updateMedianLine(List<Long> averageList){
+
+        medianLineChart.setLegendVisible(false);
+        medianLineChart.setAnimated(false);
+        medianLineChart.setCreateSymbols(true);
+        medianLineChart.setAlternativeRowFillVisible(false);
+        medianLineChart.setAlternativeColumnFillVisible(false);
+        medianLineChart.setHorizontalGridLinesVisible(false);
+        medianLineChart.setVerticalGridLinesVisible(false);
+        medianLineChart.getXAxis().setVisible(false);
+        medianLineChart.getYAxis().setVisible(false);
+
+        medianLineChart.getData().clear();
+
+
+
+        String[] daysOfTheWeek = {"Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+        long average = 0;
+        long highest = 0;
+        for(Long l : averageList){
+            average +=l;
+            if(l > highest){
+                highest = l;
+            }
+        }
+        average = average/averageList.size();
+        XYChart.Series<String,Number> series = new XYChart.Series<String, Number>();
+        for(int i=0;i< daysOfTheWeek.length;i++){
+            Data<String,Number> vars = new XYChart.Data<String, Number>(daysOfTheWeek[i],averageList.get(i));
+            series.getData().add(vars);
+        }
+        medianLineChart.getData().add(series);
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     /**
      *Updates the Bar Graph on the dashboard with the weekly values of the currently logged in user
         <p>
      * Temporary uses random numbers until a storage class is available
      */
-    private void updateWeeklyChart(){ //TODO lock the XAxis values horizontal, fix/lock the weird YAxis scaling
+    private List<Long> updateWeeklyChart(){ //TODO lock the XAxis values horizontal, fix/lock the weird YAxis scaling
         //ArrayList<Object> historicUserData = new ArrayList<>();
         List<Long> averageList = new ArrayList<>();
         String[] daysOfTheWeek = {"Sunday","Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",};
@@ -94,7 +172,7 @@ public class DashController implements Initializable, IController{
             averageList.add(userDataValue);
             Data<String,Number> vars = new XYChart.Data<String, Number>(daysOfTheWeek[i],userDataValue);
             vars.setNode(createValueLabel(vars.YValueProperty()));
-            colorSwitch(vars);
+            barColorSwitch(vars);
             series.getData().add(vars);
         }
 
@@ -112,20 +190,22 @@ public class DashController implements Initializable, IController{
 
         //Debug
         System.out.println("Average: "+average);
+        System.out.println("Highest: " +highest);
         System.out.println("Displaying data");
 
         //Assign lables
         co2ThisWeekChart.setTitle("Weekly CO2 Discharge");
-        co2ThisWeekChart.getXAxis().setLabel("Day");
-        co2ThisWeekChart.getYAxis().setLabel("Value");
+        weekChartX.setLabel("Day");
+        weekChartY.setLabel("Value");
         
         //Load the data & Toggle animations to avoid 'Issues'
+        co2ThisWeekChart.setLegendVisible(false);
         co2ThisWeekChart.setAnimated(false);
         co2ThisWeekChart.getData().clear();
         co2ThisWeekChart.getData().add(series);
-        weekChartY.setUpperBound(highest+20.0);
+        //weekChartY.setUpperBound(highest+20.0);
         co2ThisWeekChart.setAnimated(true);
-        co2ThisWeekChart.setLegendVisible(false);
+        return averageList;
     }
 
     /**
@@ -142,15 +222,11 @@ public class DashController implements Initializable, IController{
         return pane;
     }
   
-    @Override
-    public void setPresets(User user){} //Empty on purpose, needed to implement the interface
-        
-
     /**
      * Generates node colors for the given XYChart data values
      * @param  data XYChart.Data
      */
-    private void colorSwitch(XYChart.Data<String, Number> data){
+    private void barColorSwitch(XYChart.Data<String, Number> data){
         Node node = data.getNode();
         if (data.getYValue().intValue() > 99) {
             node.setStyle("-fx-bar-fill: black");
