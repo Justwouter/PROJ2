5f37f9b27dde9970c7e3715d340eea2b560cad23
Barmanturbo <21035407@student.hhs.nl>
2022-05-20 14:22:11 +0200

    territorium afbakenen

--- src/main/java/com/logic/User.java
+++ src/main/java/com/logic/User.java
@@ -49,33 +49,7 @@ public class User {
     public int getRank(){
         return this.rank;
     }
-    //Made By BarmanTurbo
-    public String vergelijkPuntMetUitstoot(){ 
-        long uitstoot = 1000-point.getPoints();
-        if(uitstoot <= 1){
-            return "Je hebt nauwelijks CO2 uitgestoten!";
-        }
-        String output = "Je CO2 uitstoot komt overeen met ";
-        if(uitstoot<9){
-            output += "wat een vrachtwagen per km uitstoot";
-            return output;
-        }
-        if(uitstoot <28){
-            output += "wat een vrachtwagen uitstoot per " + (uitstoot / 9)+ " km";
-            return output;
-        }
-        if(uitstoot <20459){
-            //ja er kan staan "wat 1 bomen", en dat is niet heel mooi, maar ik heb geen zin in veel extra code
-            output += "wat " + (uitstoot / 28) + " bomen per jaar opnemen";
-            return output;
-        }
-        if(uitstoot <122000000){
-            output += "wat de Ever Given uitstoot per " + (uitstoot / 20459) + " km";
-        }else{
-            output += ""+(uitstoot/122000000)+" keer zoveel dan wat de gemeente Amsterdam uitstootte in 2020";
-        }
-
-        return output;
-    }
+    
+    
 
 }
