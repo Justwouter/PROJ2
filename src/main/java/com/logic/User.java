cfa6ce3c7d4c9430821a93471f23effd704af839
Barmanturbo <21035407@student.hhs.nl>
2022-05-24 12:03:22 +0200

    admin class, moved duurzaamsteUsersAsString

--- src/main/java/com/logic/User.java
+++ src/main/java/com/logic/User.java
@@ -54,34 +54,6 @@ public class User {
         return this.rank;
     }
 
-    //made by BarmanTurbo
-    public String duurzaamsteUsersAsString(){
-        if(isAdmin){
-            ArrayList<User> topUsers = Leaderboard.getUsers();
-            Integer maxPoints = topUsers.get(1).getPoints();
-            String highestRankingUsers = "";
-            for(User u : topUsers){
-                if(u.getPoints()==maxPoints){
-                    highestRankingUsers += u.getNaam();
-                    if(topUsers.get(topUsers.indexOf(u)+1).getPoints() == maxPoints){
-                        highestRankingUsers += ", ";
-                    }else{
-                        if(topUsers.indexOf(u)>0){
-                            highestRankingUsers += " stoten het minste CO2 uit. Zij hebben elk " + maxPoints + " punten.";
-                        }else{
-                            highestRankingUsers += " stoot het minste CO2 uit. Hij/Zij heeft " + maxPoints + " punten.";
-                        }
-                    }
-                }else{
-                    break; //saves runtime, zodat je alleen over de users met de hoogste punten loopt.
-                }
-            }
-
-            return highestRankingUsers;
-        } else {
-            return "Requires non-mafklapper (also known as admin) rights.";
-        }
-    }
 
     //made by BarmanTurbo
     public void addWeeklyPoints(){
