1480c987f7daabe66c9a972dcfe36ef461233065
Barmanturbo <21035407@student.hhs.nl>
2022-05-24 11:12:07 +0200

    duurzaamsteUsers()

--- src/main/java/com/logic/User.java
+++ src/main/java/com/logic/User.java
@@ -53,6 +53,29 @@ public class User {
     public int getRank(){
         return this.rank;
     }
+    public String duurzaamsteUsers(){
+        ArrayList<User> topUsers = Leaderboard.getUsers();
+        Integer maxPoints = topUsers.get(1).getPoints();
+        String highestRankingUsers = "";
+        for(User u : topUsers){
+            if(u.getPoints()==maxPoints){
+                highestRankingUsers += u.getNaam();
+                if(topUsers.get(topUsers.indexOf(u)+1).getPoints() == maxPoints){
+                    highestRankingUsers += ", ";
+                }else{
+                    if(topUsers.indexOf(u)>0){
+                        highestRankingUsers += " stoten het minste CO2 uit. Zij hebben elk " + maxPoints + " punten.";
+                    }else{
+                        highestRankingUsers += " stoot het minste CO2 uit. Hij/Zij heeft " + maxPoints + " punten.";
+                    }
+                }
+            }else{
+                break; //saves runtime, zodat je alleen over de users met de hoogste punten loopt.
+            }
+        }
+
+        return highestRankingUsers;
+    }
 
     //made by BarmanTurbo
     public void addWeeklyPoints(){
