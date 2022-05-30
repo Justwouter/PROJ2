42fda3c1ec31e97848f6041e77c031c28119e53c
Studeeric <21146632@student.hhs.nl>
2022-05-30 16:01:07 +0200

    Leaderboard uiterlijk aangepast.

--- src/main/java/com/gui/LeaderBoardController.java
+++ src/main/java/com/gui/LeaderBoardController.java
@@ -6,21 +6,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.skin.TableHeaderRow;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 public class LeaderBoardController extends AController implements Initializable {
 
-    private List<User> users = new ArrayList<>();
-
+    @FXML
+    public Button dbButton1;
     private User user;
 
     @FXML
@@ -37,29 +37,63 @@ public class LeaderBoardController extends AController implements Initializable 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        users = Leaderboard.getUsers();
+        fillBoard();
+        colourBoard();
+        preventRearranging();
+    }
+
+    /**
+     * Vult het boord met de data.
+     */
+    private void fillBoard(){
         rankKolom.setCellValueFactory(new PropertyValueFactory<>("rank"));
         namesKolom.setCellValueFactory(new PropertyValueFactory<>("naam"));
         puntenKolom.setCellValueFactory(new PropertyValueFactory<>("points"));
-        ObservableList<User> data = FXCollections.observableArrayList(users);
+        ObservableList<User> data = FXCollections.observableArrayList(Leaderboard.getUsers());
         leaderboard.setItems(data);
-        namesKolom.setSortable(false);
-            leaderboard.setRowFactory(tv -> new TableRow<>() {
+    }
+
+    /**
+     * Voorkomt dat de gebruiker de kolommen kan verplaatsen.
+     */
+    private void preventRearranging(){
+        leaderboard.widthProperty().addListener((ov, t, t1) -> {
+            // Get the table header
+            TableHeaderRow header = (TableHeaderRow) leaderboard.lookup("TableHeaderRow");
+            header.reorderingProperty().addListener((observable, oldValue, newValue) -> header.setReordering(false));
+        });
+    }
+
+    /**
+     * Geeft de kleuren aan het leaderboard afhankelijk van het aantal punten per rij.
+     */
+    private void colourBoard(){
+        leaderboard.setRowFactory(tv -> new TableRow<>() {
             @Override
             protected void updateItem(User item, boolean empty) {
                 super.updateItem(item, empty);
+                getStyleClass().removeAll();
                 if (item == null || item.getPoints() == null)
-                    setStyle("");
+                    getStyleClass().add("");
                 else if (item.getPoints() > 500)
-                    setStyle("-fx-background-color: green;");
+                    getStyleClass().add("good");
                 else if (item.getPoints() > 0)
-                    setStyle("-fx-background-color: orange;");
+                    getStyleClass().add("warning");
                 else if (item.getPoints() < 0)
-                    setStyle("-fx-background-color: red;");
+                    getStyleClass().add("bad");
                 else
-                    setStyle("-fx-background-color: blue");
+                    getStyleClass().add("error");
             }
         });
+    }
+
+    /**
+     * Als een kolom gesorteerd wordt, zorgt dit ervoor dat de kleuren opnieuw komen.
+     */
+    @FXML
+    private void onSort(){
+        leaderboard.refresh();
+        colourBoard();
     }
 
     @FXML
