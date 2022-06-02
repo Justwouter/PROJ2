d73227a9f3624879bab60a408cdbdd91b5dbc483
Barmanturbo <21035407@student.hhs.nl>
2022-06-02 13:48:17 +0200

    code smells duplicate code

--- src/main/java/com/logic/PuntMutatie.java
+++ src/main/java/com/logic/PuntMutatie.java
@@ -31,20 +31,16 @@ public class PuntMutatie{
     public boolean isFromLast4Weeks(){
         Calendar datumCheck = Calendar.getInstance();
         //check for last month date
-        if(datumCheck.get(Calendar.YEAR)==datum.get(Calendar.YEAR)){
-            if(datumCheck.get(Calendar.DAY_OF_YEAR)-datum.get(Calendar.DAY_OF_YEAR)<28){
-                return true;
-            } else {
-                return false;
-            }
-        }else{
-            if(datum.get(Calendar.DAY_OF_YEAR)-datumCheck.get(Calendar.DAY_OF_YEAR)>328&&datumCheck.get(Calendar.YEAR)-datum.get(Calendar.YEAR)==1){
-                //Stel, het is nu 3 januari 2040 en de entry om te checken komtvan 28 december 2039, dan keurt dit het alsnog goed.
-                //Zo niet, returnt het false.
-                return true;
-            }else{
-                return false;
-            }
+        if((datumCheck.get(Calendar.YEAR)==datum.get(Calendar.YEAR))
+        &&(
+            (datumCheck.get(Calendar.DAY_OF_YEAR)-datum.get(Calendar.DAY_OF_YEAR)<28)
+            ||
+            (datum.get(Calendar.DAY_OF_YEAR)-datumCheck.get(Calendar.DAY_OF_YEAR)>328&&datumCheck.get(Calendar.YEAR)-datum.get(Calendar.YEAR)==1)
+        )
+        ){
+            return true;
+        } else{
+            return false;
         }
     }
 
