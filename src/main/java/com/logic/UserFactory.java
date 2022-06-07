cdeb243f8a7edc148d81d62b8256a23e16e1cbc8
Justwouter <85436941+Justwouter@users.noreply.github.com>
2022-06-07 11:22:29 +0200

    Delete UserFactory.java

--- src/main/java/com/logic/UserFactory.java
+++
@@ -1,9 +0,0 @@
-package com.logic;
-
-public abstract class UserFactory {
-    public static final NormaleUserFactory normaleUserFactory = new NormaleUserFactory();
-    public static final AdminUserFactory adminUserFactory = new AdminUserFactory();
-
-    public abstract User creatUser();
-    
-}
