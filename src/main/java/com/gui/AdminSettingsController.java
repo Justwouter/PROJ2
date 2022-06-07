30139e2502ed2094e069cb36462fe148d45328a1
Justwouter <wouter2312@gmail.com>
2022-06-07 10:35:06 +0200

    Imports removed

--- src/main/java/com/gui/AdminSettingsController.java
+++ src/main/java/com/gui/AdminSettingsController.java
@@ -1,19 +1,17 @@
 package com.gui;
 
 import com.logic.User;
+import com.logic.Leaderboard;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 
-import com.logic.Leaderboard;
-import com.logic.User;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
