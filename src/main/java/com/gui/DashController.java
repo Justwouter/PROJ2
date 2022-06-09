08fee7e7c27fd32195598e8cbd5ad63d430407e6
Justwouter <wouter2312@gmail.com>
2022-06-09 16:22:17 +0200

    Removed unused stuff

--- src/main/java/com/gui/DashController.java
+++ src/main/java/com/gui/DashController.java
@@ -1,11 +1,7 @@
 package com.gui;
 
 import com.logic.SaveManager;
-import com.logic.User;
 
-import javafx.animation.KeyFrame;
-import javafx.animation.KeyValue;
-import javafx.animation.Timeline;
 
 import javafx.beans.binding.ObjectExpression;
 import javafx.fxml.FXML;
@@ -19,10 +15,8 @@ import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
-import javafx.util.Duration;
 
 import java.io.File;
-import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -59,23 +53,6 @@ public class DashController extends AController implements Initializable {
     @FXML
     private CategoryAxis weekChartX = new CategoryAxis();
 
-
-    //Animation
-    private void exitAnim(String fxml, User user) {
-        KeyValue keyValue = new KeyValue(dashMainPane.rotateProperty(), 360);
-        // over the course of 5 seconds
-        KeyFrame keyFrame = new KeyFrame(Duration.seconds(0.5), keyValue);
-        Timeline timeline = new Timeline(keyFrame);
-        timeline.play();
-        timeline.setOnFinished(e -> {
-            try {
-                Main.show(fxml, user);
-            }
-            catch(IOException e2){
-                System.out.println(e2);
-            }
-        });
-    }
   
     //Parent methods overrides
     @Override
